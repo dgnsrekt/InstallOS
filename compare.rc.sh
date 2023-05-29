79b17e178376ca54b6eccc02fc7da5f0b66e9e62
dgnsrekt <dgnsrekt@pm.me>
2023-05-28 22:51:00 -0500

    remove: unused files

--- compare.rc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-diff .bashrc ~/.bashrc
-diff .zshrc ~/.zshrc
-diff .SpaceVim.d/init.toml ~/.SpaceVim.d/init.toml
--- copy_to_system.rc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp .bashrc ~/.bashrc 
-cp .zshrc ~/.zshrc 
-cp .SpaceVim.d/init.toml ~/.SpaceVim.d/init.toml 
